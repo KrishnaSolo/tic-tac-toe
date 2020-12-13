9e8957a26349cf8c4a1077bd71a6c626ddd8ecaa
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2020-12-12 22:17:17 -0500

    Refactored hard code

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,10 @@
 #include "Platform/Platform.hpp"
-const GAMEBOARD_SIZE = 1200;
+#define CALC_Y(l, b) (l * b)
+const int GAMEBOARD_SIZE = 1200;
+const int LINE_THICKNESS = 50;
+
+void drawBoard(sf::RenderWindow&);
+std::vector<sf::RectangleShape> setupBoard();
 
 int main()
 {
@@ -15,44 +20,52 @@ int main()
 	// Use the screenScalingFactor
 	window.create(sf::VideoMode(GAMEBOARD_SIZE * screenScalingFactor, GAMEBOARD_SIZE * screenScalingFactor), "Tic-Tac-Toe");
 
-
 	sf::Event event;
-	sf::RectangleShape horizontal_line_a(Vector2f(GAMEBOARD_SIZE, 100));
-	horizontal_line_a.setPosition(0,GAMEBOARD_SIZE/3);
-	
-	sf::RectangleShape horizontal_line_b(Vector2f(GAMEBOARD_SIZE, 100));
-	horizontal_line_b.setPosition(0,2*GAMEBOARD_SIZE/3);
-	
-	sf::RectangleShape vertical_line_a(Vector2f(100, GAMEBOARD_SIZE));
-	vertical_line_a.setPosition(GAMEBOARD_SIZE/3, 0);
-	
-	sf::RectangleShape vertical_line_b(Vector2f(100, GAMEBOARD_SIZE));
-	vertical_line_b.setPosition(2*GAMEBOARD_SIZE/3, 0);
-
 	while (window.isOpen())
 	{
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed) window.close();
+			if (event.type == sf::Event::Closed)
+				window.close();
 			if (event.type == sf::Event::MouseButtonPressed)
 			{
-    				if (event.mouseButton.button == sf::Mouse::Right)
-    				{
-        				std::cout << "the right button was pressed" << std::endl;
-        				std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-        				std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-    				}
+				if (event.mouseButton.button == sf::Mouse::Right)
+				{
+					std::cout << "the right button was pressed" << std::endl;
+					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+				}
 			}
-			
 		}
-
-		window.clear();
-		window.draw(vertical_line_a);
-		window.draw(vertical_line_b);
-		window.draw(horizontal_line_a);
-		window.draw(horizontal_line_b);
-		window.display();
+		drawBoard(window);
 	}
 
 	return 0;
+}
+
+std::vector<sf::RectangleShape> setupBoard()
+{
+	std::vector<sf::RectangleShape> board_lines;
+	for (int i = 0; i < 2; i++)
+	{
+		sf::RectangleShape line(sf::Vector2f(GAMEBOARD_SIZE, LINE_THICKNESS));
+		line.setPosition(0, (i + 1) * GAMEBOARD_SIZE / 3 - 25);
+		board_lines.push_back(line);
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		sf::RectangleShape line(sf::Vector2f(LINE_THICKNESS, GAMEBOARD_SIZE));
+		line.setPosition((i + 1) * GAMEBOARD_SIZE / 3 - 25, 0);
+		board_lines.push_back(line);
+	}
+	return board_lines;
+}
+
+void drawBoard(sf::RenderWindow& mainGameWindow)
+{
+	mainGameWindow.clear();
+	std::vector<sf::RectangleShape> board_lines = setupBoard();
+	for (sf::Shape& line : board_lines)
+		mainGameWindow.draw(line);
+	mainGameWindow.display();
 }
