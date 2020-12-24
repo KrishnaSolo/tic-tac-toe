76854f89b873e587f11e835147e09fa6f7dd5d3f
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2020-12-23 19:05:10 -0500

    Add header for tic tac toe class

---
+++ src/TicTacToe.h
@@ -0,0 +1,51 @@
+#define CALC_LINE_POS(i) ((i + 1) * gameBoard.TILE_SIZE + 50 * i)
+
+struct Board
+{
+	const int GAMEBOARD_SIZE = 1300;
+	const float DIAGONAL = 1838.47;
+	const int TILE_SIZE = 400;
+	const int LINE_THICKNESS = 50;
+	const sf::Color TILE_COLOUR = sf::Color(128, 128, 128);
+};
+
+struct Position
+{
+	float x_pos;
+	float y_pos;
+	Position(float x, float y) :
+		x_pos(x),
+		y_pos(y)
+	{}
+};
+
+struct GameData
+{
+	int turn = 0;
+	int moveCnt = 0;
+	bool change = false;
+	bool gameOver = false;
+};
+
+class TicTacToe
+{
+public:
+	TicTacToe(sf::RenderWindow& window);
+	void setupBoard();
+	void makeMove(const int x, const int y);
+	void refreshBoard();
+	bool isGameOver();
+
+private:
+	const Board gameBoard;
+	const GameData gameData;
+	sf::RenderWindow* gameWindow;
+	std::vector<sf::RectangleShape&> tiles;
+	std::vector<sf::RectangleShape&> board;
+
+	void drawTiles();
+	void drawBoard();
+	void checkWinner();
+	void drawWinner();
+	void drawLine(const Position& pos, const float h, const float w);
+};
